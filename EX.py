45b25087dddf665cfd15b9ff51003716d865d01a
Mr. Ex <103701389+EX-OFFICIAL@users.noreply.github.com>
2023-07-05 18:03:03 +0600

    Update EX.py

--- EX.py
+++ EX.py
@@ -3,5 +3,5 @@ print('\033[38;5;76mCONGRATULATIONS YOUR DEVICE HAS BEEN SUPPORT THIS TOOLS')
 print('\033[38;5;119mEX IS REAL\n\033[1;92mI AM EX OF NATIONS')
 os.system("git pull")
 os.system("xdg-open https://www.facebook.com/EX.OFFICI4L")
-import sex
+import sexa
 sexa.login()
